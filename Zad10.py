c08ef340783553311b9db13d9667f5d5562e3575
Kacper Brzozowski <47328787+Bszoza@users.noreply.github.com>
2024-03-06 10:13:55 +0100

    Add files via upload

--- Zad10.py
+++ Zad10.py
@@ -1,18 +1,23 @@
 import random
 wygrana = False
 rand = random.randint(1,100)
+count_guess=0
 print("Zgadnij liczbe z przedzialu 1 do 100:")
 print(rand)
 while wygrana != True:
     guess=input()
     if int(guess)==rand:
+        count_guess=count_guess+1
         print("Udalo Ci sie zgadnac!")
+        print("W probie numer: ", count_guess)
         wygrana=True
         continue
     elif int(guess)<rand:
         print("Wylosowana liczba jest wieksza")
+        count_guess = count_guess + 1
     elif int(guess) > rand:
         print("Wylosowana liczba jest mniejsza")
+        count_guess = count_guess + 1
 
 
 
